9d8bd62110cd896c169cc1bce9103f3dce184cf1
codeCampWithSifat <sayedhossainsifat300@.com>
2023-04-04 07:55:48 +0600

    module 75 already done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 // use all the middleware
 app.use(express.json());
@@ -15,6 +16,23 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+// verify jesonwebtoken
+function verifyJWT (req,res,next) {
+    const authHeader = req.headers.authorization ;
+    if(!authHeader) {
+        return res.status(403).send({message: "UnAuthorized Access Access"})
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err,decoded) {
+        if(err) {
+            return res.status(401).send({message: "Forbidden Access"})
+        }
+        req.decoded = decoded
+        next();
+    })
+
+}
 
 
 async function run() {
@@ -59,12 +77,62 @@ async function run() {
     });
 
     // MyAppointment.js
-    app.get("/bookings",async(req,res) => {
+    app.get("/bookings",verifyJWT,async(req,res) => {
         const email = req.query.email;
+        const decodedEmail = req.decoded.email;
+        if(email !== decodedEmail) {
+            return res.status(403).send({message: "Forbidden Access"})
+        }
+        console.log(decodedEmail);
         const query = {email:email};
         const bookings = await bookingsCollection.find(query).toArray();
         res.send(bookings);
     })
+
+    // SignUp.js
+    app.post("/users", async(req,res) => {
+        const user = req.body ;
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    });
+
+    // google Login.js and SignUp.js
+    app.put("/users/:email", async(req,res) => {
+        const email = req.params.email;
+        const updateUser = req.body;
+        const filter = {email:email,};
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+                email : updateUser.email,
+                name: updateUser.name,
+            },
+          };
+
+          const result = await usersCollection.updateOne(filter,updateDoc,options);
+        //   console.log(result);
+          res.send(result);
+    });
+
+    // get all the users on the AllUsers.js
+    app.get("/users", async(req,res) => {
+        const query = {};
+        const cursor = await usersCollection.find(query).toArray();
+        res.send(cursor)
+    })
+
+    // create a jsonwebtoken
+    app.get("/jwt", async(req,res) => {
+        const email = req.query.email;
+        const query = {email:email};
+        const user = await usersCollection.findOne(query);
+        if(user) {
+            const token = jwt.sign({email}, process.env.ACCESS_TOKEN)
+            return res.send({accessToken:token})
+        }
+        res.status(403).send({accessToken : "Forbidden Access"})
+    })
+
 
 
   } finally {
